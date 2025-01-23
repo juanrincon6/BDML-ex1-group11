f97db9fa678154ab07d8e56f43fa3e5e36fb01f8
CamiloRamirezAs <ca.ramirez10@uniandes.edu.co>
2025-01-22 19:31:22 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -13,3 +13,17 @@ db %>% select(maxEducLevel,maxEducLevel1, maxEducLevel2,maxEducLevel3 ,maxEducLe
 linear_imput_model <- lm(y_salary_m ~ ingtot + sex   + maxEducLevel3 + maxEducLevel4 + maxEducLevel5 + maxEducLevel6 + maxEducLevel7 , data = db)
 summary(linear_imput_model)
 
+db$predicted_y <- predict(linear_imput_model, newdata = db)
+
+db %>% select(y_salary_m, predicted_y, y_salary_m  ) %>% head() 
+
+## Adding missing grouping variables: `directorio`
+
+db<-  db %>%  mutate(y_salary_m = ifelse(is.na(y_salary_m) == TRUE, predicted_y , y_salary_m))
+
+db %>% select(y_salary_m, predicted_y, y_salary_m  ) %>% head() 
+
+## Adding missing grouping variables: `directorio`
+
+## drop recently created variables
+db<-  db %>% select(- maxEducLevel1, - maxEducLevel2, - maxEducLevel3, - maxEducLevel4, - maxEducLevel5, - maxEducLevel6, - maxEducLevel7,-predicted_y )
