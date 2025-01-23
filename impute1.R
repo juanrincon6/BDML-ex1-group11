53acedabe0a2844c71af2871886001cee5e3702f
anamarojas2308 <98784698+anamarojas2308@users.noreply.github.com>
2025-01-22 19:33:01 -0500

    Impute1.R G11

--- impute1.R
+++ impute1.R
@@ -1,1 +1,10 @@
-# Include here your code for your first chosen imputation method
+
+# Calcular la moda de y_salary_m
+mode_y_salary_m <- as.numeric(names(sort(table(db$y_salary_m), decreasing = TRUE)[1]))
+
+# Imputar valores faltantes de y_salary_m con la moda
+db <- db %>%
+  mutate(y_salary_m = ifelse(is.na(y_salary_m), mode_y_salary_m, y_salary_m))
+
+# Verificar que no haya valores faltantes
+sum(is.na(db$y_salary_m))
