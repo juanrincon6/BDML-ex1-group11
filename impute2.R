ae52379fbc416efb1b6c9f1f992881ede05de676
CamiloRamirezAs <ca.ramirez10@uniandes.edu.co>
2025-01-22 19:30:03 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -1,1 +1,15 @@
-# Include your code here for you second imputation method
+# transform categorical to factor
+
+db$maxEducLevel<- factor(db$maxEducLevel)
+
+dummy_maxEducLevel <- as.data.frame(model.matrix(~ maxEducLevel - 1, data = db)) 
+#db<- db  %>% cbind(dummy_maxEducLevel) 
+db <- cbind(db, dummy_maxEducLevel)
+
+db %>% select(maxEducLevel,maxEducLevel1, maxEducLevel2,maxEducLevel3 ,maxEducLevel4 ,maxEducLevel5 ,maxEducLevel6, maxEducLevel7 ) %>% head() 
+
+## Adding missing grouping variables: `directorio`
+
+linear_imput_model <- lm(y_salary_m ~ ingtot + sex   + maxEducLevel3 + maxEducLevel4 + maxEducLevel5 + maxEducLevel6 + maxEducLevel7 , data = db)
+summary(linear_imput_model)
+
